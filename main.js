470a5543e28cab3c8714782dc6f4a3b96c43cc45
Mohamed Hany <163010472+Mohamedhany1233@users.noreply.github.com>
2025-09-29 21:06:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ let x_winners_span = document.querySelector('.title .win .x-wins')
 let o_winners_span = document.querySelector('.title .win .o-wins')
 let x_wins = 0;
 let o_wins = 0;
-// console.log(title);
-
 
 //set wins in local storage
 
@@ -28,18 +26,12 @@ function theEnd(num1,num2,num3){
     whoIsWin(squares[num1])
 
     //paint background color items
-    document.getElementById('item'+num1).style.backgroundColor='#000'
-    document.getElementById('item'+num2).style.backgroundColor='#000'
-    document.getElementById('item'+num3).style.backgroundColor='#000'
+    document.getElementById('item'+num1).style.backgroundColor = '#000'
+    document.getElementById('item'+num2).style.backgroundColor = '#000'
+    document.getElementById('item'+num3).style.backgroundColor = '#000'
     //Interval and Timeout
     setInterval(function(){title.innerHTML+='.'},1000)
     setTimeout(function(){location.reload()},2000)
-}
-
-function fair(){
-    title.innerHTML = `${squares[num1].toUpperCase()} is winner`
-    setInterval(function(){title.innerHTML+='.'},1000)
-    setTimeout(function(){location.reload()},1000)
 }
 
 function winner(){
@@ -47,40 +39,51 @@ function winner(){
       squares[i]= document.getElementById('item' + i).innerHTML
     }
     //console.log(squares);
-    if(squares[1]==squares[2]&&squares[2] == squares[3] && squares[1]!=''){
+    if(squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != ''){
         theEnd(1,2,3)
-        console.log(squares)
-}
-    else if(squares[4]==squares[5] &&squares[5]== squares[6]&& squares[4]!=''){
+        draw = () => {}
+    }
+    else if(squares[4] == squares[5] && squares[5]== squares[6]&& squares[4] != ''){
         theEnd(4,5,6)
+        draw = () => {}
     }
-    else if(squares[7]==squares[8] &&squares[8]== squares[9]&& squares[7]!=''){
+    else if(squares[7] == squares[8] && squares[8]== squares[9]&& squares[7] != ''){
         theEnd(7,8,9)
+        draw = () => {}
     }
-    else if(squares[1]==squares[5] &&squares[5]== squares[9]&& squares[1]!=''){
+    else if(squares[1] == squares[5] && squares[5]== squares[9]&& squares[1] != ''){
         theEnd(1,5,9)
+        draw = () => {}
     }
-    else if(squares[3]==squares[5] &&squares[5]== squares[7]&& squares[3]!=''){
+    else if(squares[3] == squares[5] && squares[5]== squares[7]&& squares[3] != ''){
         theEnd(3,5,7)
+        draw = () => {}
     }
-    else if(squares[1]==squares[4] &&squares[4] == squares[7]&& squares[1]!=''){
+    else if(squares[1] == squares[4] && squares[4] == squares[7]&& squares[1] != ''){
         theEnd(1,4,7)
+        draw = () => {}
     }
-    else if(squares[2]==squares[5] && squares[5] == squares[8]&& squares[5]!=''){
+    else if(squares[2] == squares[5] && squares[5] == squares[8]&& squares[5] != ''){
         theEnd(2,5,8)
+        draw = () => {}
     }
-    else if(squares[3]==squares[6] &&squares[6] == squares[9]&& squares[3]!=''){
+    else if(squares[3] == squares[6] && squares[6] == squares[9]&& squares[3] != ''){
         theEnd(3,6,9)
+        draw = () => {}
     }
     //fair
-   let full = true
+    draw() 
+}
+
+function draw() {
+    let full = true
     for(let i=1; i<10; i++){
         if(squares[i] === '') {
             full = false
             break
         }
     }
-    if(full) {
+    if (full) {
         title.innerHTML = "It's a Draw!"
         setTimeout(() => location.reload(), 2000)
     }
@@ -89,12 +92,12 @@ function winner(){
 function WriteTurn(id){
     let item = document.getElementById(id)
 
-    if(turn === 'x' && item.innerHTML===''){
+    if(turn === 'x' && item.innerHTML==='') {
         item.innerHTML = 'x'      
         turn ='o'                 
         title.innerHTML=`Turn ${turn.toUpperCase()}`
     }
-    else if(turn === 'o' && item.innerHTML===''){
+    else if(turn === 'o' && item.innerHTML==='') {
         item.innerHTML = 'o'
         turn = 'x'
         title.innerHTML=`Turn ${turn.toUpperCase()}`
@@ -113,12 +116,11 @@ function whoIsWin(winner) {
         o_wins++;
         localStorage.setItem('o-wins' , JSON.stringify(o_wins))
         o_winners_span.innerHTML = o_wins
-        console.log('thats o')
     }
 }
-
 
 btn.onclick = function(){
     location.reload()
     localStorage.clear()
 }
+
